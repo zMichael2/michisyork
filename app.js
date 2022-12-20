b1a88bf94353340141a9571eacc45b126b5eb58b
zMichael2 <maikoldesigner@gmail.com>
2022-12-20 00:44:46 -0500

    v3

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get("/michis", (req, res) => {
       id: 0,
       name: "Leo",
       gender: "male",
-      imageFileName: "leo.jpeg",
+      imageFileName: "https://i.postimg.cc/1X2RNX0V/leo.jpg",
       description:
         "Leo is a supercute boy who really wants his forever home for Christmas. He is quite a chilled cat and is used to people and other cats. Come and meet Leo and give him the loving home he is yearning for.",
       age: "10 years old",
@@ -20,7 +20,7 @@ app.get("/michis", (req, res) => {
       id: 1,
       name: "Jasmine",
       gender: "female",
-      imageFileName: "jasmine.jpeg",
+      imageFileName: "https://i.postimg.cc/hjCtcpfr/jasmine.jpg",
       description:
         "Jasmine is a beautiful young girl who will make a wonderful pet to any household. She is loving and fun and gets on with other cats. Please come and meet Jasmin at the cat shelter and I am sure you will fall in love with her.",
       age: "2 years old",
@@ -29,7 +29,7 @@ app.get("/michis", (req, res) => {
       id: 2,
       name: "MARTHA",
       gender: "female",
-      imageFileName: "martha.jpeg",
+      imageFileName: "https://i.postimg.cc/xj41Zprj/martha.jpg",
       description:
         "MARTHA is a very friendly girl who has sadly come into the care of the RSPCA after her owner died. Martha is 12 years old. Martha loves all humans, both male & female but isn't too keen on children so will need an adult only household. Martha loves both indoors & outdoors.",
       age: "12 years old",
@@ -38,7 +38,7 @@ app.get("/michis", (req, res) => {
       id: 3,
       name: "Toby",
       gender: "male",
-      imageFileName: "toby.jpeg",
+      imageFileName: "https://i.postimg.cc/W1Mph2dt/toby.jpg",
       description:
         "Toby came into us as an injured stray. A member of the public reported him to us as he was struggling to eat with an apparent mouth injury and was becoming very skinny. He'd been treated and has started to put weight on again. He is super friendly and even has a love heart shaped marking on his side!",
       age: "7 years old",
@@ -47,7 +47,7 @@ app.get("/michis", (req, res) => {
       id: 4,
       name: "Vienna",
       gender: "female",
-      imageFileName: "vienna.jpeg",
+      imageFileName: "https://i.postimg.cc/7LwHzpz9/vienna.jpg",
       description:
         "Vienna was brought to us by an Animal Rescue Officer after she was found abandoned, covered in fleas and with nasty sores on her feet. Her feet have healed well and she's been treated for fleas and worms. She is very friendly and would love a new home.",
       age: "8 years old",
@@ -56,7 +56,7 @@ app.get("/michis", (req, res) => {
       id: 5,
       name: "Joey",
       gender: "male",
-      imageFileName: "joey.jpeg",
+      imageFileName: "https://i.postimg.cc/cCr4Hw3N/joey.jpg",
       description:
         "I'm a sweet older gentleman who sadly lost my home at the ripe old age of 16 and was found straying. My DOB may say I'm older but my mind and body tells me different! I get about very well and and love to potter around and see what everyone else is up to! I'm an easy going chap with people and will always come to say hello. I love my food and will happily have anything that is on offer!",
       age: "14 years old",
@@ -65,7 +65,7 @@ app.get("/michis", (req, res) => {
       id: 6,
       name: "Mary",
       gender: "female",
-      imageFileName: "mary.jpeg",
+      imageFileName: "https://i.postimg.cc/NjhfsyfW/mary.jpg",
       description:
         "Our Mary is the second of our lovely Mama cats who've raised their babies and are now on the lookout for their furever (retirement!) home! Mary and Mumma are sisters, but they don't need to be rehomed together. They've been in separate foster homes and haven't missed each other.",
       age: "around a year old",
